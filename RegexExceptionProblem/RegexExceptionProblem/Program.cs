6caa4d4ae79e146200380f517d676c1a9724d483
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-05-28 12:09:23 +0530

    [First Name regex exception problem]

--- RegexExceptionProblem/RegexExceptionProblem/Program.cs
+++ RegexExceptionProblem/RegexExceptionProblem/Program.cs
@@ -1,13 +1,23 @@
-﻿using System;
+﻿
+using System;
 
 
-namespace RegexExcepitonProblem
+namespace RegexExceptionProblem
 {
     class Program
     {
         public static void Main(string[] args) 
         {
-            Console.WriteLine("Welcome to Regex Exception Problem");
+            Console.WriteLine("Welcome to Regex Exception Problem user registration");
+
+            Console.WriteLine("hello regex problem UC-1 first name");
+
+            Console.WriteLine("Enter your First Name, Check valid or not");
+            string TakeName = Console.ReadLine();
+            RegexClass regexClass = new RegexClass();
+            regexClass.ValidFirstName(TakeName);
+
+
         }
     }
 }
---
+++ RegexExceptionProblem/RegexExceptionProblem/RegexClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace RegexExceptionProblem
+{
+    public class RegexClass
+    {
+        public static string NameRules = "^[A-Z]{1}[a-z]{2,}$";
+
+        public void ValidFirstName(string Name)
+        {
+            try
+            {
+
+
+                if (Regex.IsMatch(Name, NameRules))
+                    Console.WriteLine($"This is Valid Name:\n{Name}");
+                else
+                    Console.WriteLine($"This is Invalid:\n{Name}");
+            }
+
+            catch (Exception e)
+            {
+                
+                Console.WriteLine("Enter valid name formate");
+            }
+        }
+    }
+}
