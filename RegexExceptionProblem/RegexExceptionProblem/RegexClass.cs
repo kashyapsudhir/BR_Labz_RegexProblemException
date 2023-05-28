3e2007fe37088fb06df4c0632332523f9075ec65
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-05-28 13:00:59 +0530

    [check Password3 of UC8 regex exception problem valid or not]

--- RegexExceptionProblem/RegexExceptionProblem/RegexClass.cs
+++ RegexExceptionProblem/RegexExceptionProblem/RegexClass.cs
@@ -13,9 +13,11 @@ namespace RegexExceptionProblem
         //public static string LastNameRules = "^[A-Z]{1}[a-z]{2,}$";
         // public static string EmailRules = "^[a-z]+[.+-_]{0,1}[0-9 a-z]+[@][a-z]+[.][a-z]{2,3}([.][a-z]{2,3}){0,1}$";
         //public static string PhoneNumberRules = "^[0-9]{0,2}[+]{0,1}[8 9]{0,1}[0-9]{9}$";
-        //UC5- //public static string PasswordRules = "^[0-9 A-Z a-z]{8,}$";
+        //UC5- public static string PasswordRules = "^[0-9 A-Z a-z]{8,}$";
         //UC6- public static string PasswordRules = "^([A - Z]{0,1})[0-9A-Za-z]{8,}$";
-        public static string PasswordRules = "^[A-Z]{1,}[A-Za-z0-9]{1,10}";
+        //UC7- public static string PasswordRules = "^[A-Z]{1,}[A-Za-z0-9]{1,10}";
+        public static string PasswordRules = "^(?=[a-zA-Z0-9#@$?-_]{8,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).*$";
+
 
 
 
