ca847b7622ad85ee0cd01877b5a98d43583afec2
kashyapsudhir <kashyapsudhir1996@gmail.com>
2023-05-28 16:57:30 +0530

    [branch merge with main]

--- RegexExceptionProblem/RegexExceptionProblem/Program.cs
+++ RegexExceptionProblem/RegexExceptionProblem/Program.cs
@@ -10,35 +10,40 @@ namespace RegexExceptionProblem
         {
             Console.WriteLine("Welcome to Regex Exception Problem user registration");
 
-            Console.WriteLine("Enter your Email  UC9, Check Email valid or not");
+            
 
 
-            //Console.WriteLine("Enter your Last Name, Check valid or not");
-            //string TakeName = Console.ReadLine();
+            Console.WriteLine("Enter your First Name, Check valid or not");
+            string TakeName = Console.ReadLine();
             RegexClass regexClass = new RegexClass();
-            //create object of RegexClass
-            //regexClass.ValidFirstName(TakeName);
+                                                            //create object of RegexClass
+            regexClass.ValidFirstName(TakeName);
 
             //UC2
 
-
-            //string TakeLastName = Console.ReadLine();
-            //regexClass.ValidLastName(TakeLastName);
-
-
-            //string Email = Console.ReadLine();
-            //regexClass.ValidEmail(Email);
-
-            //string TakeNumber = Console.ReadLine();
-            //regexClass.CheckPhoneNumber(TakeNumber);
+            Console.WriteLine("Enter your Last Name, Check valid or not");
+            string TakeLastName = Console.ReadLine();
+            regexClass.ValidLastName(TakeLastName);
 
 
-            //string Password = Console.ReadLine();
-            //regexClass.CheckPassword(Password);
+            Console.WriteLine("Enter your Email, Check valid or not");
+            string Email = Console.ReadLine();
+            regexClass.ValidEmail(Email);
 
 
+            Console.WriteLine("Enter your Phone number, Check valid or not");
+            string TakeNumber = Console.ReadLine();
+            regexClass.CheckPhoneNumber(TakeNumber);
+
+
+            Console.WriteLine("Enter your Password, Check valid or not");
+            string Password = Console.ReadLine();
+            regexClass.CheckPassword(Password);
+
+
+            Console.WriteLine("Enter your EmailUC9, Check valid or not");
             string UC9Email = Console.ReadLine();
-            regexClass.ValidEmail(UC9Email);
+            regexClass.ValidEmailUC9(UC9Email);
         }
     }
 }
--- RegexExceptionProblem/RegexExceptionProblem/RegexClass.cs
+++ RegexExceptionProblem/RegexExceptionProblem/RegexClass.cs
@@ -9,112 +9,122 @@ namespace RegexExceptionProblem
 {
     public class RegexClass
     {
-        //public static string NameRules = "^[A-Z]{1}[a-z]{2,}$";
-        //public static string LastNameRules = "^[A-Z]{1}[a-z]{2,}$";
-        // public static string EmailRules = "^[a-z]+[.+-_]{0,1}[0-9 a-z]+[@][a-z]+[.][a-z]{2,3}([.][a-z]{2,3}){0,1}$";
-        //public static string PhoneNumberRules = "^[0-9]{0,2}[+]{0,1}[8 9]{0,1}[0-9]{9}$";
-        //UC5- public static string PasswordRules = "^[0-9 A-Z a-z]{8,}$";
-        //UC6- public static string PasswordRules = "^([A - Z]{0,1})[0-9A-Za-z]{8,}$";
-        //UC7- public static string PasswordRules = "^[A-Z]{1,}[A-Za-z0-9]{1,10}";
-        //UC8- public static string PasswordRules = "^(?=[a-zA-Z0-9#@$?-_]{8,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).*$";
-        public static string UC9EmailRules = (@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$");
+        public static string NameRules = "^[A-Z]{1}[a-z]{2,}$";
+        public static string LastNameRules = "^[A-Z]{1}[a-z]{2,}$";
+        public static string EmailRules = "^[a-z]+[.+-_]{0,1}[0-9 a-z]+[@][a-z]+[.][a-z]{2,3}([.][a-z]{2,3}){0,1}$";
+        public static string PhoneNumberRules = "^[0-9]{0,2}[+]{0,1}[8 9]{0,1}[0-9]{9}$";
+       // public static string PasswordRules = "^[0-9 A-Z a-z]{8,}$";//UC5
+        //public static string PasswordRules1 = "^([A - Z]{0,1})[0-9A-Za-z]{8,}$";//UC6
+        //public static string PasswordRules2 = "^[A-Z]{1,}[A-Za-z0-9]{1,10}";//UC7
+        public static string PasswordRules = "^(?=[a-zA-Z0-9#@$?-_]{8,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).*$";//UC8
+        public static string UC9EmailRules = (@"^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*#?&])[A-Za-z\d@$!%*#?&]{8,}$");//UC9
 
 
 
         //UC2
 
-        //public void ValidFirstName(string Name)
-        //{
-        //    try
-        //    {
+        public void ValidFirstName(string Name)
+        {
+            try
+            {
 
 
-        //        if (Regex.IsMatch(Name, NameRules))
-        //            Console.WriteLine($"This is Valid Name:\n{Name}");
-        //        else
-        //            Console.WriteLine($"This is Invalid:\n{Name}");
-        //    }
+                if (Regex.IsMatch(Name, NameRules))
+                    Console.WriteLine($"This is Valid Name:\n{Name}");
+                else
+                    Console.WriteLine($"This is Invalid:\n{Name}");
+            }
 
-        //    catch (Exception e)
-        //    {
+            catch (Exception e)
+            {
 
-        //        Console.WriteLine("Enter valid name formate");
-        //    }
-        //}
+                Console.WriteLine("Enter valid name formate");
+            }
+        }
 
         //UC2
 
-        //public void ValidLastName(string LastName)
-        //{
-        //    try
-        //    {
+        public void ValidLastName(string LastName)
+        {
+            try
+            {
 
 
-        //        if (Regex.IsMatch(LastName, LastNameRules))
-        //            Console.WriteLine($"This is Valid LastName:\n{LastName}");
-        //        else
-        //            Console.WriteLine($"This is Invalid LastName:\n{LastName}");
-        //    }
+                if (Regex.IsMatch(LastName, LastNameRules))
+                    Console.WriteLine($"This is Valid LastName:\n{LastName}");
+                else
+                    Console.WriteLine($"This is Invalid LastName:\n{LastName}");
+            }
 
-        //    catch (Exception e)
-        //    {
+            catch (Exception e)
+            {
 
-        //        Console.WriteLine("Enter valid Last Name formate");
-        //    }
-        //}
+                Console.WriteLine("Enter valid Last Name formate");
+            }
+        }
 
         //UC3
 
-        //public void ValidEmail(string EmailCheck)
-        //{
+        public void ValidEmail(string EmailCheck)
+        {
+            try
+            {
 
-        //    if (Regex.IsMatch(EmailCheck, EmailRules))
-        //        Console.WriteLine($"This is Valid Email:{EmailCheck}");
-        //    else
-        //        Console.WriteLine($"This is Invalid Email:{EmailCheck}");
-        //}
+
+                if (Regex.IsMatch(EmailCheck, EmailRules))
+                    Console.WriteLine($"This is Valid Email:{EmailCheck}");
+                else
+                    Console.WriteLine($"This is Invalid Email:{EmailCheck}");
+            }
+
+            catch(Exception e) 
+            {
+                Console.WriteLine("enter valid email");
+            }
+        }
 
 
         //UC4
 
-        //public void CheckPhoneNumber(string PhoneNumber)
-        //{
-        //    try
-        //    {
+        public void CheckPhoneNumber(string PhoneNumber)
+        {
+            try
+            {
 
 
-        //        if (Regex.IsMatch(PhoneNumber, PhoneNumberRules))
-        //            Console.WriteLine($"This is Valid PhoneNumber:{PhoneNumber}");
-        //        else
-        //            Console.WriteLine($"This is Invalid PhoneNumber:{PhoneNumber}");
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Console.WriteLine("Please Enter Valid Mobile Number");
-        //    }
+                if (Regex.IsMatch(PhoneNumber, PhoneNumberRules))
+                    Console.WriteLine($"This is Valid PhoneNumber:{PhoneNumber}");
+                else
+                    Console.WriteLine($"This is Invalid PhoneNumber:{PhoneNumber}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Please Enter Valid Mobile Number");
+            }
 
-        //}
+        }
 
         //UC5
-        //public void CheckPassword(string Password)
-        //{
-        //    try
-        //    {
 
-        //        if (Regex.IsMatch(Password, PasswordRules))
-        //            Console.WriteLine($"This is Valid Password:{Password}");
-        //        else
-        //            Console.WriteLine($"This is Invalid Password:{Password}");
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        Console.WriteLine("Please enter valid password");
-        //    }
-        //}
+        public void CheckPassword(string Password)
+        {
+            try
+            {
+
+                if (Regex.IsMatch(Password, PasswordRules))
+                    Console.WriteLine($"This is Valid Password:{Password}");
+                else
+                    Console.WriteLine($"This is Invalid Password:{Password}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Please enter valid password");
+            }
+        }
 
         //UC9
 
-        public void ValidEmail(string EmailCheck)
+        public void ValidEmailUC9(string EmailCheck)
         {
             try
             {
